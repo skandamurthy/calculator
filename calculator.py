2d33a84d3466a79438733a9260a607db601e113d
Skandamurthy <skandasneha@gmail.com>
2018-07-28 14:58:36 +0530

    Added power function using Inheritence

--- calculator.py
+++ calculator.py
@@ -4,26 +4,30 @@ class calculator():
         self.name = name
 
     def add(self,a,b):
-        return (a+b)
+        return a + b
 
     def subtract(self,a,b):
-        return (a-b)
+        return a - b
 
     def multiply (self,a,b):
-        return (a*b)
+        return a * b
 
     def divide(self,a,b):
         if b ==0:
             print("Enter a non-divisible number")
             return 0
         else:
-            return(a/b)
+            return a / b
+
+class ScientificCalculator(calculator):
+    def power(self,a):
+        return a * a
 
 
 a=int(input("Enter First number:"))
 b =int(input("Enter Second number:"))
 function = input("Math Fuction:")
-obj1 = calculator("calculator1")
+obj1 = ScientificCalculator("calculator1")
 if function =="+":
     print(obj1.add(a,b))
 elif function =='-':
@@ -32,6 +36,8 @@ elif function =='*':
     print(obj1.multiply(a,b))
 elif function =='/':
     print(obj1.divide(a,b))
+elif function =='power':
+    print(obj1.power(a))
 else:
     print("Invalid Function")
 
